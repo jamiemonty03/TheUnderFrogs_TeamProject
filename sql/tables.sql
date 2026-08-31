5247a74589919c1eb8000d916dce8b701801128c
Shane <sginty123@gmail.com>
2026-08-31 13:52:39 +0000

    Adding order table to tables.sql

--- sql/tables.sql
+++ sql/tables.sql
@@ -6,3 +6,14 @@ CREATE TABLE instruments (
     currency    CHAR(3) NOT NULL,
     tradable    BOOLEAN NOT NULL
 );
+
+CREATE TABLE orders (
+    order_id         CHAR(36) NOT NULL PRIMARY KEY,
+    account_id       BIGINT NOT NULL REFERENCES accounts(account_id),
+    symbol           VARCHAR(20)  NOT NULL REFERENCES instruments(symbol),
+    side             VARCHAR(4) NOT NULL,
+    quantity         INT NOT NULL,
+    price            INT NOT NULL,
+    order_status     VARCHAR(20) NOT NULL,
+    idempotency_key  VARCHAR(100) UNIQUE
+);
