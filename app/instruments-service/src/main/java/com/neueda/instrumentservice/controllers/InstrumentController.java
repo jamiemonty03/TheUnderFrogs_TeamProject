d4ef4087f3b7daa123750edee2d5a905cf2c2c69
david <davidjayakumar2005@outlook.com>
2026-09-22 13:14:54 +0000

    feat: Wiring instrument controller in to real instrument data

--- app/instruments-service/src/main/java/com/neueda/instrumentservice/controllers/InstrumentController.java
+++ app/instruments-service/src/main/java/com/neueda/instrumentservice/controllers/InstrumentController.java
@@ -1,14 +1,33 @@
-
 package com.neueda.instrumentservice.controllers;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.neueda.instrumentservice.dtos.responses.InstrumentResponse;
+import com.neueda.instrumentservice.exceptions.InstrumentNotFoundException;
+import com.neueda.instrumentservice.services.InstrumentService;
 
 @RestController
 @RequestMapping("/instruments")
-    public class InstrumentController {
+public class InstrumentController {
+
+    private final InstrumentService instrumentService;
+
+    public InstrumentController(InstrumentService instrumentService) {
+        this.instrumentService = instrumentService;
+    }
+
     @GetMapping
-    public String getInstruments() {
-        return "List of instruments";
+    public ResponseEntity<List<InstrumentResponse>> getInstruments() {
+        return ResponseEntity.ok(instrumentService.getAllInstruments());
+    }
+
+    @GetMapping("/{symbol}")
+    public ResponseEntity<InstrumentResponse> getInstrument(@PathVariable String symbol)
+            throws InstrumentNotFoundException {
+        return ResponseEntity.ok(instrumentService.getInstrumentBySymbol(symbol));
     }
 }
