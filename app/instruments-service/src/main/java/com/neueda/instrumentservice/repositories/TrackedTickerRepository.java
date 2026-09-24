98f3da668d4ba71c44dd17c30ea1fba990e8c1e9
david <davidjayakumar2005@outlook.com>
2026-09-24 09:10:17 +0000

    refactro: changed from repository to mapper to follow mybatis preferences

--- app/instruments-service/src/main/java/com/neueda/instrumentservice/repositories/TrackedTickerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.neueda.instrumentservice.repositories;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TrackedTickerRepository {
-    int deactivate(String symbol);
-}
